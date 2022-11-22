beecf8df656c5692e7afdb72bacfeea54b4ca182
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-11-21 21:01:43 -0600

    added event edit functionality

--- utils/data/eventData.js
+++ utils/data/eventData.js
@@ -7,6 +7,22 @@ const getEvents = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const getSingleEvent = (eventId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events/${eventId}`)
+    .then((response) => resolve(response.json()))
+    .catch((error) => reject(error));
+});
+
+const updateEvent = (data, id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+    .then((response) => resolve(response))
+    .catch((error) => reject(error));
+});
+
 const createEvent = (event) => new Promise((resolve, reject) => {
   fetch(`${clientCredentials.databaseURL}/events`, {
     method: 'POST',
@@ -20,4 +36,6 @@ const createEvent = (event) => new Promise((resolve, reject) => {
 });
 
 // eslint-disable-next-line import/prefer-default-export
-export { getEvents, createEvent };
+export {
+  getEvents, createEvent, getSingleEvent, updateEvent,
+};
